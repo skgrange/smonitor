8faf41b32bb1e207b9645476015fb3df4c88486e
skgrange <s.k.grange@gmail.com>
2016-04-14 11:35:24 +0100

    add import_sites

--- R/importing_functions.R
+++ R/importing_functions.R
@@ -328,3 +328,27 @@ import_summaries <- function(con, extra = TRUE) {
   df
   
 }
+
+
+
+#' Function to import sites table from a \strong{smonitor} database. 
+#' 
+#' @param con Database connection. 
+#' @param extra Return extra data? Default is \code{TRUE}.
+#' 
+#' @export
+import_sites <- function(con, extra = TRUE) {
+  
+  # Get look-up table
+  df <- threadr::db_get(con, "SELECT sites.*
+                              FROM sites")
+  
+  # Only a few variables
+  if (!extra)
+    df <- df[, c("site", "site_name", "latitude", "longitude", "elevation", 
+                 "region", "site_type")]
+  
+  # Return
+  df
+  
+}
