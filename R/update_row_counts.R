8f05fb80c3ad73a6fa50ca092910dce7b17d6746
skgrange <s.k.grange@gmail.com>
2016-06-30 12:06:28 +0100

    add databaser to tavis build

--- R/update_row_counts.R
+++ R/update_row_counts.R
@@ -3,14 +3,20 @@
 #' @author Stuart K. Grange
 #' 
 #' @param con Database connection. 
+#' 
+#' @param estimate Should the row counts be estimated rather than counted? Only
+#' works for PostgreSQL databases.
+#'  
 #' @param read Should the table be immediately read after being updated? Default
 #' is \code{FALSE}. 
 #' 
+#' @seealso \code{\link{db_count_rows}}
+#' 
 #' @export
-update_row_counts <- function(con, read = FALSE) {
+update_row_counts <- function(con, estimate = FALSE, read = FALSE) {
   
   # Get counts of all tables
-  df <- databaser::db_count_rows(con, table = NA)
+  df <- databaser::db_count_rows(con, table = NA, estimate)
   
   # Insert and always overwrite
   databaser::db_insert(con, "row_counts", df, append = FALSE, overwrite = TRUE)
