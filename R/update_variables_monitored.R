e06beb43bf4ec1653501e6e76305860b3a8cc2f3
skgrange <s.k.grange@gmail.com>
2018-08-27 17:48:13 +0100

    :sparkles: enhance update_variables_monitored

--- R/update_variables_monitored.R
+++ R/update_variables_monitored.R
@@ -1,5 +1,5 @@
 #' Function to update the \code{variables_monitored} variable in the 
-#' \code{`process`} table of an \strong{smonitor} database. 
+#' \code{`site`} table of an \strong{smonitor} database. 
 #' 
 #' @param con Database connection. 
 #' 
@@ -10,21 +10,19 @@
 #' @export
 update_variables_monitored <- function(con) {
   
-  # Get look up table
-  df_processes <- import_processes(con)
-  
-  # Nest variables
-  df <- df_processes %>% 
+  # Summarise process table
+  df <- import_processes(con) %>% 
+    arrange(site,
+            variable) %>% 
     group_by(site) %>% 
-    do(nest_variable_vectors(.)) %>% 
+    summarise(variables_monitored = stringr::str_c(unique(variable), collapse = "; "),
+              observation_count = sum(observation_count, na.rm = TRUE)) %>% 
     ungroup() %>% 
-    mutate(variables_monitored = ifelse(
-      observation_count == 0, NA, variables_monitored
-    ),
-    variables_monitored = stringr::str_replace_na(variables_monitored))
+    mutate(variables_monitored = ifelse(observation_count == 0, NA, variables_monitored),
+           variables_monitored = stringr::str_replace_na(variables_monitored))
   
   # Build some sql
-  sql <- stringr::str_c(
+  sql_update <- stringr::str_c(
     "UPDATE sites SET variables_monitored='", 
     df$variables_monitored, "'
     WHERE site='", 
@@ -32,35 +30,12 @@ update_variables_monitored <- function(con) {
   )
   
   # Clean
-  sql <- stringr::str_replace_all(sql, "'NA'", "NULL")
-  sql <- threadr::str_trim_many_spaces(sql)
+  sql_update <- stringr::str_replace_all(sql_update, "'NA'", "NULL")
+  sql_update <- stringr::str_squish(sql_update)
   
   # Do
-  databaser::db_execute(con, sql)
+  databaser::db_execute(con, sql_update)
   
   # No return
-  
-}
-
-
-# Nest the vector
-nest_variable_vectors <- function(df) {
-  
-  # Get variables
-  variables <- sort(unique(df$variable))
-  variables <- stringr::str_c(variables, collapse = "; ")
-  
-  # Get observation count too
-  observation_count <- sum(as.numeric(df$observation_count), na.rm = TRUE)
-  
-  # Build data frame
-  df <- data.frame(
-    site = df$site[1],
-    variables_monitored = variables,
-    observation_count = observation_count,
-    stringsAsFactors = FALSE
-  )
-  
-  return(df)
   
 }
