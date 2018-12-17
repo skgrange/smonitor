cfb1382472dd0192845364049b478dfd15d89f41
skgrange <s.k.grange@gmail.com>
2018-12-17 10:07:39 +0000

    :sparkles: move update_validity to purrr and tidy up a bit

--- R/update_validity.R
+++ R/update_validity.R
@@ -1,104 +1,159 @@
 #' Function to update validity variable in a \code{`observations`} table in a 
 #' \strong{smonitor} database. 
 #' 
-#' \code{update_validity} does not really update the validity variable, it queries
-#' the database, re-tests, deletes the old observations and inserts the re-tested
-#' table. 
-#' 
-#' @author Stuart K. Grange
+#' \code{update_validity} does not update the validity variable with SQL, it 
+#' queries the database, re-tests, deletes the old observations and inserts the
+#' re-tested table. 
 #' 
 #' @param con Database connection. 
 #' 
 #' @param process A vector of processes. 
 #' 
 #' @param summary Summary to invalidate. This will usually be \code{0} for source
-#' data. 
+#' data and can only take one value. 
 #' 
-#' @param tz Time-zone. 
+#' @param tz Time zone to conduct invalidations in. 
 #' 
-#' @param delete Type of delete-match to use for dates.
+#' @param verbose Should the function give messages? 
 #' 
-#' @param progress Progress bar type. Default is \code{"time"}.
+#' @author Stuart K. Grange
+#' 
+#' @return Invisible \code{con}.
 #' 
 #' @export
 update_validity <- function(con, process, summary = 0, tz = "UTC", 
-                            delete = "between", progress = "time") {
+                            verbose = FALSE) {
   
-  # Check
-  if (!delete %in% c("between", "all")) 
-    stop("'delete' must be 'between' or 'all'.", call. = FALSE)
-  
-  if (!length(summary) == 1) 
-    stop("Only one summary can be used.", call. = FALSE)
+  # Check input
+  if (!length(summary) == 1) stop("Only one summary can be used.", call. = FALSE)
   
   # Get look-up table
-  df_look <- import_invalidations(con, tz = tz) %>% 
+  df <- import_invalidations(con, tz = tz) %>% 
     mutate(date_start = as.numeric(date_start),
-           date_end = as.numeric(date_end))
+           date_end = as.numeric(date_end),
+           summary = summary)
   
   # Filter
-  df_look <- df_look %>% 
-    filter(process %in% !!process)
+  df <- filter(df, process %in% !!process)
   
   # Check again
-  if (nrow(df_look) == 0) 
-    stop("Processes have no entries in the `invalidation` table.", call. = FALSE)
+  if (nrow(df) != 0) {
+    
+    # Do
+    df %>% 
+      split(list(.$process, .$summary)) %>% 
+      purrr::walk(
+        ~update_validity_worker(
+          con,
+          df = .x,
+          verbose = verbose
+        )
+      )
+    
+  } else {
+    
+    if (verbose) {
+      
+      message(
+        threadr::date_message(), 
+        "`invalidations` table contains no entires for given processes..."
+      )
+      
+    }
+    
+  }
   
-  # Do for every process
-  plyr::l_ply(process, function(x) 
-    update_validity_worker(
-      con, 
-      process = x, 
-      summary = summary, 
-      df_look = df_look, 
-      delete = delete
-    ), 
-    .progress = progress
-  )
-  
-  # No return
+  return(invisible(con))
   
 }
 
 
 # No export needed
-update_validity_worker <- function(con, process, summary, df_look, delete) {
+update_validity_worker <- function(con, df, verbose) {
+  
+  # Get keys, will be single values from split input
+  process <- df$process[1]
+  summary <- df$summary[1]
   
   # Get observations
-  # Catch is for when database contains no data for a process and gives an error
-  df <- tryCatch({
+  if (verbose) message(threadr::date_message(), "Importing observations...")
+  df_observations <- import_by_process(
+    con, 
+    process, 
+    summary = summary, 
+    start = 1900, 
+    end = format(Sys.Date() + lubridate::years(5)), 
+    valid_only = FALSE,
+    warn = FALSE
+  )
+  
+  if (nrow(df_observations) != 0) {
     
-    import_by_process(
-      con, 
-      process, 
-      summary = summary, 
-      start = 1965, 
-      end = 2020, 
-      valid_only = FALSE
-    ) %>% 
+    # To unix dates
+    df_observations <- df_observations %>% 
       mutate(date = as.numeric(date),
              date_end = as.numeric(date_end))
     
-  }, error = function(e) {
-    
-    # Return no observations
-    data.frame()
-    
-  })
-  
-  if (nrow(df) != 0) {
-    
     # Update validity, look up table is filtered in function
-    df <- validity_test(df, df_look)
+    if (verbose) message(threadr::date_message(), "Applying invalidation...")
+    df_observations <- smonitor_observation_validity_test(df_observations, df)
     
     # Delete old observations
-    delete_observations(con, df, match = delete)
+    delete_observations(con, df_observations, match = "between", verbose = verbose)
     
     # Insert new observations
-    insert_observations(con, df)
+    insert_observations(con, df_observations, verbose = verbose)
+    
+  } else {
+    
+    if (verbose) message(threadr::date_message(), "No observations to invalidate...")
     
   }
   
-  # No return
+}
+
+
+#' Function to add a validity variable to a data frame based on date ranges. 
+#' 
+#' @param df_observations Data frame to test and add or overwrite a 
+#' \code{validity} variable. 
+#' 
+#' @param df A look-up data frame containing \code{process}, \code{date_start}, 
+#' and \code{date_end} variables.
+#' 
+#' @param valid_value For values which are not invalid, \emph{i.e.} they fail 
+#' the date range test, what value should they take? Default is \code{NA}, but
+#' \code{1} could be appropriate too.  
+#' 
+#' @seealso \code{\link{within_range}}
+#' 
+#' @author Stuart K. Grange. 
+#' 
+#' @export
+smonitor_observation_validity_test <- function(df_observations, df, 
+                                               valid_value = as.integer(NA)) {
+  
+  # Get keys from table
+  # Get identifier
+  process <- df_observations$process[1]
+  
+  # Filter look up table
+  df <- filter(df, process == !!process)
+  
+  if (nrow(df) != 0) {
+    
+    # Apply invalidation
+    df_observations <- df_observations %>% 
+      mutate(test = threadr::within_range(date, df$date_start, df$date_end),
+             validity = if_else(test, as.integer(0), valid_value)) %>% 
+      select(-test)
+    
+  } else {
+    
+    df_observations$validity <- valid_value
+    
+  }
+  
+  return(df_observations)
   
 }
--- R/zzz.R
+++ R/zzz.R
@@ -7,7 +7,7 @@ if (getRversion() >= "2.15.1") {
     "date_start", "code", "site_name", "process", "validity", "date_start",
     "variables_monitored", "significant", "envirologger_station", "service",
     "envirologger_channel_number", "site_code", "date_local", "date_unix",
-    "software"
+    "software", "df_look", "test"
   )
   
   # Squash the notes
