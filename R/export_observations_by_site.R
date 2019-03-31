61c3c9204a996e983932600b6ded5c6d8fee83ae
skgrange <s.k.grange@gmail.com>
2019-03-31 16:18:15 +0100

    :sparkles: enhance tidy exporter so year-site files can be exported & add fs to depends

--- R/export_observations_by_site.R
+++ R/export_observations_by_site.R
@@ -24,6 +24,9 @@
 #' @param file_type Type of files to export. Can be either \code{".csv.gz"} or 
 #' \code{".rds"}. 
 #' 
+#' @param by_year Should files be exported for the site-year combinations? This 
+#' will create directories for each year too. 
+#' 
 #' @param verbose Should the function give messages? 
 #' 
 #' @return Invisible \code{con} and exported files.
@@ -32,7 +35,8 @@
 export_observations_by_site <- function(con, df, start = 1960, end = NA, 
                                         site_name = FALSE, directory_output, 
                                         file_name_prefix = NA_character_,
-                                        file_type = ".csv.gz", verbose = FALSE) {
+                                        file_type = ".csv.gz", by_year = FALSE, 
+                                        verbose = FALSE) {
   
   # Check data frame input
   stopifnot("process" %in% names(df))
@@ -55,10 +59,6 @@ export_observations_by_site <- function(con, df, start = 1960, end = NA,
   if (is.na(end)) end <- lubridate::year(lubridate::now())
   end <- threadr::parse_date_arguments(end, "end")
   
-  # For function
-  start <- format(start)
-  end <- format(end)
-  
   # Do by site
   df %>% 
     split(.$site, drop = TRUE) %>% 
@@ -66,12 +66,13 @@ export_observations_by_site <- function(con, df, start = 1960, end = NA,
       ~export_observations_by_site_worker(
         con, 
         df = .x,
-        start = start,
-        end = end, 
+        start = format(start),
+        end = format(end), 
         site_name = site_name,
         directory_output = directory_output,
         file_name_prefix = file_name_prefix,
         file_type = file_type,
+        by_year = by_year,
         verbose = verbose
       )
     )
@@ -84,7 +85,7 @@ export_observations_by_site <- function(con, df, start = 1960, end = NA,
 # Define the worker
 export_observations_by_site_worker <- function(con, df, start, end, site_name,
                                                directory_output, file_name_prefix, 
-                                               file_type, verbose) {
+                                               file_type, by_year, verbose) {
   
   # Query database for observations
   if (verbose) {
@@ -108,16 +109,45 @@ export_observations_by_site_worker <- function(con, df, start, end, site_name,
       message(threadr::date_message(), "Exporting `", file_type, "` data file...")
     }
     
-    # Build output file name, no extension yet
-    file_output <- stringr::str_c(file_name_prefix, df$site[1])
-    file_output <- file.path(directory_output, file_output)
-    file_output <- stringr::str_c(file_output, file_type)
-    
-    # Export
-    if (file_type == ".rds") {
-      saveRDS(df, file_output)
-    } else if (file_type == ".csv.gz") {
-      readr::write_csv(df, file_output)
+    if (by_year) {
+      
+      if (verbose) {
+        message(threadr::date_message(), "Exporting year-site files...")
+      }
+      
+      # Split data frame into year chunks
+      list_df <- df %>% 
+        mutate(year = lubridate::year(date)) %>% 
+        dplyr::group_split(year)
+      
+      # Export by site and year
+      purrr::walk(
+        list_df,
+        ~export_observations_by_site_and_year_worker(
+          df = .x, 
+          directory_output = directory_output,
+          file_name_prefix = file_name_prefix,
+          file_type = file_type
+        )
+      )
+      
+    } else {
+      
+      # Build output file name
+      file_output <- stringr::str_c(file_name_prefix, df$site[1])
+      file_output <- fs::path(directory_output, file_output) 
+      # Add file extention
+      file_output <- stringr::str_c(file_output, file_type)
+      # Ensure directory is there
+      fs::dir_create(fs::path_dir(file_output))
+      
+      # Export
+      if (file_type == ".rds") {
+        saveRDS(df, file_output)
+      } else if (file_type == ".csv.gz") {
+        readr::write_csv(df, file_output)
+      }
+      
     }
     
   } else{
@@ -129,5 +159,33 @@ export_observations_by_site_worker <- function(con, df, start, end, site_name,
   }
   
   return(invisible(con))
+  
+}
+
+
+export_observations_by_site_and_year_worker <- function(df, directory_output, 
+                                                        file_name_prefix, 
+                                                        file_type) {
+  
+  # Add year directory and year identifier in file name
+  year <- df$year[1]
+  file_output <- stringr::str_c(file_name_prefix, df$site[1], "_", year)
+  file_output <- fs::path(directory_output, year, file_output)
+  # Add file extention
+  file_output <- stringr::str_c(file_output, file_type)
+  # Ensure directory is there
+  fs::dir_create(fs::path_dir(file_output))
+  
+  # Drop year from data frame
+  df <- select(df, -year)
+  
+  # Export
+  if (file_type == ".rds") {
+    saveRDS(df, file_output)
+  } else if (file_type == ".csv.gz") {
+    readr::write_csv(df, file_output)
+  }
+  
+  return(invisible(df))
   
 }
