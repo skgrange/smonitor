aae4456d530d7c60273dd2b413b2b89486eeb06d
skgrange <s.k.grange@gmail.com>
2019-04-02 11:55:59 +0100

    :sparkles: catch checker in update_process_spans

--- R/update_process_spans.R
+++ R/update_process_spans.R
@@ -7,24 +7,59 @@
 #' @param process A process vector to update. Default is \code{NA} which will 
 #' cause the database to update all process keys. 
 #' 
+#' @param by_process Should the database observations spans be calculated 
+#' individually for each process? 
+#' 
 #' @param na.rm Should missing values (\code{NA}/\code{NULL}) be omited from the
 #' aggregation functions? 
+#' 
+#' @param verbose Should the function give messages? 
 #' 
 #' @author Stuart K. Grange
 #' 
 #' @return Invisible \code{con}. 
 #' 
 #' @export
-update_process_spans <- function(con, process = NA, na.rm = FALSE) {
+update_process_spans <- function(con, process = NA, by_process = FALSE, 
+                                 na.rm = TRUE, verbose = FALSE) {
+  
+  # Parse input
+  process <- process %>% 
+    unique() %>% 
+    sort()
+  
+  # Check for empty inputs
+  stopifnot(is.na(process) && length(process) >= 1)
+  
+  if (is.na(process) && verbose) {
+    message(
+      threadr::date_message(), 
+      "All processes will be updated, this can be a slow process..."
+    )
+  }
+  
+  if (verbose) {
+    message(
+      threadr::date_message(), 
+      "Calculating ", 
+      length(process), 
+      " process spans..."
+    )
+  }
   
   # Calculate process spans
   df <- calculate_process_spans(
     con, 
     process = process, 
+    by_process = by_process,
     na.rm = na.rm
   ) 
   
   if (nrow(df) != 0) {
+    
+    if (verbose) {
+      message(threadr::date_message(), "Updating database...")
+    }
     
     df <- df %>% 
       mutate(date_start = stringr::str_replace_na(date_start), 
@@ -33,17 +68,13 @@ update_process_spans <- function(con, process = NA, na.rm = FALSE) {
     # Build update statements
     sql <- stringr::str_c(
       "UPDATE processes
-     SET date_start=", df$date_start, 
+       SET date_start=", df$date_start, 
       ",date_end=", df$date_end, 
       ",observation_count=", df$observation_count,
       " WHERE process=", df$process, ""
-    )
-    
-    # No quotes for NULL
-    sql <- stringr::str_replace_all(sql, "NA", "NULL")
-    
-    # Clean
-    sql <- threadr::str_trim_many_spaces(sql)
+    ) %>% 
+      stringr::str_replace_all("NA", "NULL") %>% 
+      stringr::str_squish()
     
     # Update variables to be null before insert, only when all processes are 
     # Done
@@ -59,6 +90,10 @@ update_process_spans <- function(con, process = NA, na.rm = FALSE) {
     # Use statements
     databaser::db_execute(con, sql)
     
+  } else {
+    if (verbose) {
+      message(threadr::date_message(), "No database updates done...")
+    }
   }
   
   return(invisible(con))
@@ -66,7 +101,7 @@ update_process_spans <- function(con, process = NA, na.rm = FALSE) {
 }
 
 
-calculate_process_spans <- function(con, process, na.rm) {
+calculate_process_spans <- function(con, process, by_process, na.rm) {
   
   # Build sql
   if (is.na(process[1])) {
@@ -81,17 +116,33 @@ calculate_process_spans <- function(con, process, na.rm) {
     
   } else {
     
-    sql <- stringr::str_c(
-      "SELECT process, 
-      MIN(date) AS date_start, 
-      MAX(date) AS date_end,
-      CAST(COUNT(*) AS REAL) AS observation_count
-      FROM observations", 
-      " WHERE process IN (", 
-      stringr::str_c(process, collapse = ","),
-      ") GROUP BY process 
-      ORDER BY process"
-    )
+    if (!by_process) {
+      
+      sql <- stringr::str_c(
+        "SELECT process, 
+        MIN(date) AS date_start, 
+        MAX(date) AS date_end,
+        CAST(COUNT(*) AS REAL) AS observation_count
+        FROM observations", 
+        " WHERE process IN (", 
+        stringr::str_c(process, collapse = ","),
+        ") GROUP BY process 
+        ORDER BY process"
+      )
+      
+    } else {
+      
+      sql <- stringr::str_c(
+        "SELECT process, 
+        MIN(date) AS date_start, 
+        MAX(date) AS date_end,
+        CAST(COUNT(*) AS REAL) AS observation_count
+        FROM observations", 
+        " WHERE process=", 
+        process
+      )
+      
+    }
     
   }
   
@@ -113,31 +164,34 @@ calculate_process_spans <- function(con, process, na.rm) {
   sql <- stringr::str_squish(sql)
   
   # Get table
-  df <- databaser::db_get(con, sql)
-  
-  # If no processes are given, do all the processes
-  if (is.na(process[1])) {
-    
-    # sql group by will not return groups with no data
-    df_processes <- databaser::db_get(
-      con,
-      "SELECT process
-       FROM processes
-       ORDER BY process"
-    )
-
-    # Join and add observation counts if missing
-    df <- df_processes %>%
-      left_join(df, by = "process") %>%
-      mutate(
-        observation_count = ifelse(
-          is.na(observation_count),
-          0,
-          observation_count)
-      )
-    
+  if (length(sql) == 1) {
+    df <- databaser::db_get(con, sql)
+  } else{
+    df <- purrr::map_dfr(sql, ~databaser::db_get(con, .x))
   }
   
+  # sql group by will not return groups with no data
+  # Could be enhanced to minimise query database less, but need another logic
+  # switch
+  df_processes <- databaser::db_get(
+    con,
+    "SELECT process
+    FROM processes
+    ORDER BY process"
+  )
+  
+  # Join and add observation counts if missing
+  df <- df_processes %>%
+    left_join(df, by = "process") %>%
+    mutate(
+      observation_count = if_else(
+        is.na(observation_count),
+        0,
+        observation_count)
+    )
+  
+  if (!is.na(process[1])) df <- filter(df, process %in% !!process)
+    
   return(df)
   
 }
