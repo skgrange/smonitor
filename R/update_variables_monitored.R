49c61b5e81deff6b4dd00ce494ae91d94f315f9a
skgrange <s.k.grange@gmail.com>
2019-10-25 08:59:11 +0200

    :sparkles: enhance update_variables_monitored to correctly handle missing-ness and add some messaging

--- R/update_variables_monitored.R
+++ R/update_variables_monitored.R
@@ -5,14 +5,18 @@
 #' 
 #' @param site Vector of sites to update. 
 #' 
+#' @param verbose Should the function give messages? 
+#' 
 #' @author Stuart K. Grange
 #' 
 #' @return Invisible \code{con}. 
 #' 
 #' @export
-update_variables_monitored <- function(con, site = NA) {
+update_variables_monitored <- function(con, site = NA, verbose = FALSE) {
   
   # Get process entries
+  if (verbose) message(threadr::date_message(), "Querying `processes`...")
+  
   df <- databaser::db_get(
     con, 
     "SELECT site,
@@ -25,6 +29,8 @@ update_variables_monitored <- function(con, site = NA) {
   if (!is.na(site[1])) df <- filter(df, site %in% !!site)
   
   # Summarise
+  if (verbose) message(threadr::date_message(), "Summarising processes...")
+  
   df <- df %>% 
     arrange(site,
             variable) %>% 
@@ -32,13 +38,13 @@ update_variables_monitored <- function(con, site = NA) {
     summarise(variables_monitored = stringr::str_c(unique(variable), collapse = "; "),
               observation_count = sum(observation_count, na.rm = TRUE)) %>% 
     ungroup() %>% 
-    mutate(variables_monitored = if_else(
-      observation_count == 0, 
-      NA_character_, variables_monitored
-    ),
-    variables_monitored = stringr::str_replace_na(variables_monitored))
+    mutate(
+      variables_monitored = if_else(observation_count == 0, NA_character_, variables_monitored)
+    )
   
   # Build some sql and use
+  if (verbose) message(threadr::date_message(), "Updating `sites`...")
+  
   df %>% 
     databaser::build_update_statements("sites", ., where = "site", squish = TRUE) %>% 
     databaser::db_execute(con, .)
