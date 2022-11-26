bd700b5574c4cd3509692f8f6fb0e482e689af34
Stuart Grange <s.k.grange@gmail.com>
2022-11-26 18:28:58 +0100

    :sparkles: add smonitor_details function

---
+++ R/smonitor_details.R
@@ -0,0 +1,30 @@
+#' Function to get details for a \strong{smonitor} database. 
+#' 
+#' @param con Database connection for a \strong{smonitor} database. 
+#' 
+#' @param task A character vector to indicate what task is being (or has been) 
+#' done. 
+#' 
+#' @author Stuart K. Grange
+#' 
+#' @return Tibble. 
+#' 
+#' @export
+smonitor_details <- function(con, task = NA_character_) {
+  
+  # Check if tables exist
+  stopifnot(databaser::db_table_exists(con, c("sites", "processes", "observations")))
+  
+  tibble(
+    date = round(as.numeric(lubridate::now())),
+    system = threadr::hostname(),
+    db_name = databaser::db_name(con),
+    db_class = databaser::db_class(con),
+    task = task,
+    db_size = databaser::db_size(con, unit = "mb"),
+    n_sites = databaser::db_get(con, "SELECT COUNT(*) AS n FROM sites")$n,
+    n_processes = databaser::db_get(con, "SELECT COUNT(*) AS n FROM processes")$n,
+    n_observations = databaser::db_get(con, "SELECT COUNT(*) AS n FROM observations")$n
+  )
+  
+}
