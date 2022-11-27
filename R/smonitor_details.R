0a278ad038d7677ebb1e4132e671cb1b2ca5c05d
Stuart Grange <s.k.grange@gmail.com>
2022-11-27 08:34:02 +0100

    :sparkles: enhance smonitor_details to return number of tables too

--- R/smonitor_details.R
+++ R/smonitor_details.R
@@ -16,12 +16,13 @@ smonitor_details <- function(con, task = NA_character_) {
   stopifnot(databaser::db_table_exists(con, c("sites", "processes", "observations")))
   
   tibble(
-    date = round(as.numeric(lubridate::now())),
     system = threadr::hostname(),
+    date = round(as.numeric(lubridate::now())),
     db_name = databaser::db_name(con),
     db_class = databaser::db_class(con),
     task = task,
     db_size = databaser::db_size(con, unit = "mb"),
+    n_tables = length(databaser::db_list_tables(con)),
     n_sites = databaser::db_get(con, "SELECT COUNT(*) AS n FROM sites")$n,
     n_processes = databaser::db_get(con, "SELECT COUNT(*) AS n FROM processes")$n,
     n_observations = databaser::db_get(con, "SELECT COUNT(*) AS n FROM observations")$n
