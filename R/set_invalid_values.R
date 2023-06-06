abec63bb0dd5dd657e9d27814815deb7969c2a12
skgrange <skgrange@gmail.com>
2023-06-06 11:30:41 +0200

    :sparkles: enhance set_invalid_values with a include_zero argument

--- R/set_invalid_values.R
+++ R/set_invalid_values.R
@@ -12,7 +12,20 @@
 #' 
 #' @param validity An integer vector representing validity. 
 #' 
+#' @param include_zero Should validities of \code{0} be considered invalid and
+#' the corresponding values be set to \code{NA}? \code{0} is used for calibration
+#' activities and sometimes these observations are needed, but other invalidity
+#' types are not.
+#' 
 #' @export
-set_invalid_values <- function(value, validity) {
-  if_else(validity <= 0L & !is.na(validity), NA_real_, as.numeric(value))
+set_invalid_values <- function(value, validity, include_zero = TRUE) {
+  
+  if (include_zero) {
+    value <- if_else(validity <= 0L & !is.na(validity), NA_real_, as.numeric(value))
+  } else {
+    value <- if_else(validity <= -1L & !is.na(validity), NA_real_, as.numeric(value))
+  }
+  
+  return(value)
+  
 }
