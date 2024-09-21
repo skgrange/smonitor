5315ae3eb72ba1a4d730c5751cf537937a9e4189
skgrange <s.k.grange@gmail.com>
2024-09-21 08:53:44 +0200

    :sparkles: smonitor_details now returns more detailed operating system string

--- R/smonitor_details.R
+++ R/smonitor_details.R
@@ -31,7 +31,7 @@ smonitor_details <- function(con, task = NA_character_) {
   # Get high level database and details
   df <- tibble(
     system = threadr::hostname(),
-    operating_system = stringr::str_to_lower(Sys.info()["sysname"]),
+    operating_system = get_os_release_string(),
     date = round(as.numeric(lubridate::now())),
     db_name = databaser::db_name(con),
     db_class = databaser::db_class(con),
@@ -74,5 +74,82 @@ get_db_row_count <- function(con, table, tables) {
   }
   
   return(x)
+  
+}
+
+
+# Pulled from systemr
+get_os_release_string <- function(add_prefix = TRUE) {
+  
+  # Get os name from R
+  os_system_name <- stringr::str_to_lower(Sys.info()["sysname"])
+  
+  if (os_system_name == "windows") {
+    
+    # Get system info with system call
+    df <- get_windows_system_info()
+    
+    # Build release string
+    x <- stringr::str_c(df$value[1], " (", df$value[2], ")")
+    
+  } else if (os_system_name == "linux") {
+    
+    # Get release info by reading file
+    df <- get_os_release("/etc/os-release")
+    
+    # Get the string that is desired
+    x <- df %>% 
+      filter(stringr::str_detect(variable, "(?i)pretty")) %>% 
+      pull(value)
+    
+  }
+  
+  # Add prefix if desired
+  if (add_prefix) {
+    x <- stringr::str_c(os_system_name, ":", x)
+  }
+  
+  return(x)
+  
+}
+
+
+get_os_release <- function(file = "/etc/os-release") {
+  
+  if (fs::file_exists(file)) {
+    
+    # Read release file as text
+    text <- readLines(file)
+    
+    # Format to a tibble
+    df <- text %>% 
+      stringr::str_remove_all('"') %>% 
+      stringr::str_split_fixed("=", n = 2) %>% 
+      as_tibble(.name_repair = "minimal") %>% 
+      purrr::set_names(c("variable", "value"))
+    
+  } else {
+    df <- tibble()
+  }
+  
+  return(df)
+  
+}
+
+
+get_windows_system_info <- function() {
+  
+  # Run programme, a slow call and `ver` is not available it seems
+  list_run <- processx::run("systeminfo")
+  
+  # Extract and format return
+  list_run %>% 
+    .[["stdout"]] %>% 
+    stringr::str_split_1("\n") %>% 
+    stringr::str_subset("^OS Name|^OS Version") %>% 
+    stringr::str_squish() %>% 
+    stringr::str_split_fixed(": ", n = 2) %>% 
+    as_tibble(.name_repair = "minimal") %>% 
+    purrr::set_names(c("variable", "value"))
   
 }
